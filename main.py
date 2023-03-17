2df6a02aa62ec4024ce3a597bcc749818464fd6f
Nikita <118022034+kisaphcik@users.noreply.github.com>
2023-03-17 17:25:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-from bs4 import BeautifulSoup
-import sqlite3
+from flask import Flask, request
+import json
 
-db = sqlite3.connect('PC_database.db')
-cur = db.cursor()
-cur.execute("""CREATE TABLE data(
-    name TEXT,
-    price TEXT,
-    link TEXT
-)""")
+app = Flask(__name__)
 
+@app.route('/post', methods=['POST'])
+def main():
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
 
-def func(page):
-    with open(f"html_DNS/{page}.html", 'r', encoding="utf8") as f:
-        soup = BeautifulSoup(f, 'lxml')
-
-        for el in soup.select(".catalog-products > .catalog-product"):
-            name = el.select(".catalog-product__name > span")
-            link = el.find('a').get('href')
-            price = el.select(".product-buy__price-wrap > .product-buy__price")[0].text.split("₽")
-            cur.execute("INSERT INTO data VALUES(?, ?, ?)", (name[0].text, price[0], f"https://www.dns-shop.ru{link}"))
+    handle_dialog(response, request.json)
+    return json.dumps(response)
 
 
-for i in range(1, 13):
-    func(i)
+def handle_dialog(res, req):
+    if req["session"]["new"]:
+        req["response"]["text"] = "Привет"
+    else:
+        if req["request"]["original_utterance"].lower() == "привет":
+            req["response"]["text"] = "Как дела?"
 
-db.commit()
-db.close()
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
