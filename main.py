25409cbfa914a1ae0ebc85f1845ba34059738807
dan2400 <128154600+dan2400@users.noreply.github.com>
2023-03-27 21:29:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from flask import Flask, request
-import json
-
-app = Flask(__name__)
-
-@app.route('/post', methods=['POST'])
-def main():
-    response = {
-        'session': request.json['session'],
-        'version': request.json['version'],
-        'response': {
-            'end_session': False
-        }
-    }
-
-    handle_dialog(response, request.json)
-    return json.dumps(response)
+num = [i for i in range(0, 101)]
+print(num[len(num) // 2])
 
 
-def handle_dialog(res, req):
-    if req["session"]["new"]:
-        req["response"]["text"] = "Привет"
+def bisearch(dif):
+    global num
+    if len(num) > 1:
+        if dif == '<':
+            num = num[0:len(num) // 2]
+            return num[len(num) // 2]
+        elif dif == '>':
+            num = num[(len(num) // 2) + 1:]
+            return num[len(num) // 2]
     else:
-        if req["request"]["original_utterance"].lower() == "привет":
-            req["response"]["text"] = "Как дела?"
+        print(num[0])
 
 
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+b = input()
+while b != '=':
+    print(bisearch(b))
+    b = input()
